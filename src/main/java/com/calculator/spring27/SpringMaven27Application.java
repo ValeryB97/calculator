73468325facbd64e74fed89cd3bf64c3c33780b0
ValeryB97 <valery.babaryko@gmail.com>
2024-06-16 22:59:58 +0700

    Calculator.

---
+++ src/main/java/com/calculator/spring27/SpringMaven27Application.java
@@ -0,0 +1,13 @@
+package com.calculator.spring27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaven27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMaven27Application.class, args);
+	}
+
+}
